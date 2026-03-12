55c241a4580f24917f689220903b7993b8acb910
zettezet <dtue68@gmail.com>
2026-03-12 11:36:23 +0700

    update test

--- web_ban_tra_sua/models/SanPham.php
+++ web_ban_tra_sua/models/SanPham.php
@@ -1,25 +1,28 @@
 <?php
-    class SanPham {
-        public $conn; //khai báo phương thức
+class SanPham
+{
+    public $conn; //khai báo phương thức
 
-        public function __construct()
-        {
-            $this->conn = connectDB();
+    public function __construct()
+    {
+        $this->conn = connectDB();
+    }
+
+    // viết hàm lấy toàn bộ danh sách sp
+    public function getALLProduct()
+    {
+        try {
+            $spl = 'SELECT * FROM san_phams';
+
+            $stmt = $this->conn->prepare($spl);
+
+            $stmt->execute();
+
+            return $stmt->fetchALL();
+        } catch (Exception $e) {
+            echo 'Lõi' . $e->getMessage();
         }
-
-        // viết hàm lấy toàn bộ danh sách sp
-        public function getALLProduct(){
-            try{
-                $spl = 'SELECT * FROM san_phams';
-
-                $stmt = $this->conn->prepare($spl);
-
-                $stmt->execute();
-
-                return $stmt->fetchALL();
-            } catch(Exception $e) {
-                echo 'Lõi' . $e->getMessage();
-            }
-        }
+    }
 }
 ?>
+// test pull request
